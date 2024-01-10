5baf8bece6665834b910bcf5a8528f6dffb3256d
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-10 09:13:38 -0300

    Working with Modifier

--- app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
+                    CustomPreview()
                 }
             }
         }
@@ -65,19 +71,50 @@ fun BoxPreview() {
 @Preview(showBackground = true)
 @Composable
 fun CustomPreview() {
-    Column {
+    Column(
+        Modifier
+            .padding(8.dp)
+            .background(Color.Blue)
+            .padding(8.dp)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
         Text(text = "Text 1")
         Text(text = "Text 2")
-        Row {
+        Row(
+            modifier = Modifier
+                .padding(
+                    horizontal = 8.dp,
+                    vertical = 16.dp
+                )
+                .background(Color.Green)
+                .fillMaxWidth(0.9f)
+        ) {
             Text(text = "Text 3")
             Text(text = "Text 4")
         }
-        Box {
-            Row {
+        Box(
+            Modifier
+                .padding(8.dp)
+                .background(color = Color.Red)
+                .padding(all = 8.dp)
+        ) {
+            Row(
+                Modifier
+                    .padding(8.dp)
+                    .background(color = Color.Cyan)
+                    .padding(all = 8.dp)
+                    .fillMaxWidth()
+            ) {
                 Text(text = "Text 5")
                 Text(text = "Text 6")
             }
-            Column {
+            Column(
+                Modifier
+                    .padding(8.dp)
+                    .background(color = Color.Yellow)
+                    .padding(all = 8.dp)
+            ) {
                 Text(text = "Text 7")
                 Text(text = "Text 8")
             }
