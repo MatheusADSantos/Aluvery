7de01a58ec8e702f3ff79728d66470b14440119e
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-24 14:55:16 -0300

    Challenge to use same technics ...

--- app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
@@ -3,20 +3,25 @@ package com.github.matheusadsantos.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,8 +29,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
@@ -41,7 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductItem()
+                    CardItem()
                 }
             }
         }
@@ -50,39 +55,42 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun ProductItem() {
-    Surface(Modifier.padding(8.dp),
-            shape = RoundedCornerShape(25.dp),
-            shadowElevation = 4.dp) {
-        Column(Modifier
-                .heightIn(250.dp, 300.dp)
-                .width(200.dp)) {
-            val sizeImage = 100.dp
+fun CardItem() {
+    val sizeImage = 100.dp
+    Surface(Modifier.padding(8.dp), shadowElevation = 4.dp, shape = RoundedCornerShape(8.dp)) {
+        Row(Modifier
+                .height(sizeImage * 2)
+                .fillMaxWidth()) {
             Box(Modifier
-                    .height(100.dp)
-                    .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
-                    .fillMaxWidth()) {
+                    .width(sizeImage)
+                    .fillMaxHeight()
+                    .background(brush = Brush.verticalGradient(colors = listOf(
+                            Purple500, Teal200
+                    )))) {
                 Image(
                         painter = painterResource(id = R.drawable.ic_launcher_background),
                         contentDescription = null,
                         Modifier
                                 .size(sizeImage)
-                                .offset(y = sizeImage / 2)
-                                .clip(shape = CircleShape)
                                 .align(Alignment.Center)
+                                .offset(x = sizeImage / 2)
+                                .clip(CircleShape)
+                                .border(BorderStroke(2.dp,
+                                        Brush.verticalGradient(listOf(Purple500, Teal200))),
+                                        CircleShape)
                 )
             }
-            Spacer(modifier = Modifier.height(sizeImage / 2))
-            Column(Modifier.padding(16.dp)) {
-                Text(text = LoremIpsum(50).values.first(),
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight(700),
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis)
-                Text(text = "R$ 14,99",
-                        Modifier.padding(top = 8.dp),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight(400))
+            Spacer(modifier = Modifier.width(sizeImage / 2))
+            Box(Modifier
+                    .padding(16.dp)
+                    .fillMaxHeight()
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)) {
+                Column {
+                    Text(text = LoremIpsum(words = 50).values.first(),
+                            overflow = TextOverflow.Ellipsis,
+                            lineHeight = 20.sp)
+                }
             }
         }
     }
