437930fb821c3f1f493f05ca00b8437ae509ac22
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-24 11:35:20 -0300

    Finalizing card item

--- app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -49,35 +51,39 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun ProductItem() {
-    val sizeImage = 100.dp
-    Column(Modifier
-            .height(250.dp)
-            .width(200.dp)) {
-        Box(Modifier
-                .height(100.dp)
-                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
-                .fillMaxWidth()) {
-            Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = null,
-                    Modifier
-                            .size(sizeImage)
-                            .offset(y = sizeImage / 2)
-                            .clip(shape = CircleShape)
-                            .align(Alignment.Center)
-            )
-        }
-        Spacer(modifier = Modifier.height(50.dp))
-        Column(Modifier.padding(16.dp)) {
-            Text(text = LoremIpsum(50).values.first(),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight(700),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis)
-            Text(text = "R$ 14,99",
-                    Modifier.padding(top = 8.dp),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight(400))
+    Surface(Modifier.padding(8.dp),
+            shape = RoundedCornerShape(25.dp),
+            shadowElevation = 4.dp) {
+        Column(Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)) {
+            val sizeImage = 100.dp
+            Box(Modifier
+                    .height(100.dp)
+                    .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
+                    .fillMaxWidth()) {
+                Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = null,
+                        Modifier
+                                .size(sizeImage)
+                                .offset(y = sizeImage / 2)
+                                .clip(shape = CircleShape)
+                                .align(Alignment.Center)
+                )
+            }
+            Spacer(modifier = Modifier.height(sizeImage / 2))
+            Column(Modifier.padding(16.dp)) {
+                Text(text = LoremIpsum(50).values.first(),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight(700),
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis)
+                Text(text = "R$ 14,99",
+                        Modifier.padding(top = 8.dp),
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight(400))
+            }
         }
     }
 }
