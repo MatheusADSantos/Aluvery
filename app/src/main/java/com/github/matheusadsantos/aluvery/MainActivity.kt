a5c7b1054f570ffe109feb8fbbfff964024058d0
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-24 00:30:24 -0300

    Applying gradient, alignment and displacement

--- app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.github.matheusadsantos.aluvery
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,23 +7,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
+import com.github.matheusadsantos.aluvery.ui.theme.Purple500
+import com.github.matheusadsantos.aluvery.ui.theme.PurpleGrey80
+import com.github.matheusadsantos.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,16 +45,23 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun ProductItem() {
-    Column {
-        Box(modifier = Modifier
-            .width(50.dp)
-            .background(Color.Blue)
-            .height(70.dp)) {
+    Column(Modifier
+            .width(250.dp)
+            .height(200.dp)) {
+        Box(Modifier
+                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
+                .height(100.dp)
+                .fillMaxWidth()) {
 
         }
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                Modifier
+                        .size(100.dp)
+                        .offset(y = (-50).dp)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.CenterHorizontally)
         )
         Text(text = "Text 1")
         Text(text = "Text 2")
