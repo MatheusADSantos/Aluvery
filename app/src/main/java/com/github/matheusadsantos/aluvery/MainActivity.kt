b6081ccacf5b9ef8a3a5afdde7276d8b89d2d6c3
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-24 22:49:17 -0300

    Creating product session with componentization

--- app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -41,11 +42,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductItem()
+                    ProductSession()
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ProductSession() {
+      Column {
+          Text(text = "Promotions")
+          Row {
+              ProductItem()
+              ProductItem()
+              ProductItem()
+          }
+      }
 }
 
 @Preview
