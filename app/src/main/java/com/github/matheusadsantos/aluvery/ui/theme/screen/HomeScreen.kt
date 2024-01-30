6c6b4d97e88710a9f5250e359aef7891223e0e4f
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-30 10:48:27 -0300

    Implemented Lazy Columns in product section

--- app/src/main/java/com/github/matheusadsantos/aluvery/ui/theme/screen/HomeScreen.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/theme/screen/HomeScreen.kt
@@ -1,42 +1,38 @@
 package com.github.matheusadsantos.aluvery.ui.theme.screen
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.matheusadsantos.aluvery.R
 import com.github.matheusadsantos.aluvery.model.Product
-import com.github.matheusadsantos.aluvery.sampledata.sampleProducts
 import com.github.matheusadsantos.aluvery.sampledata.sampleSections
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
 import com.github.matheusadsantos.aluvery.ui.theme.component.ProductsSection
-import java.math.BigDecimal
 
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    Column(
+    LazyColumn(
         Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
             .padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         for (section in sections) {
             val title = section.key
             val products = section.value
-            ProductsSection(
-                title = title,
-                products = products
-            )
+            item {
+                ProductsSection(
+                    title = title,
+                    products = products
+                )
+            }
         }
     }
 }
