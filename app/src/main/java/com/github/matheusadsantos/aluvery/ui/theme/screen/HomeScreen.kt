771a990d07e6434728c5b227d0ee2eda53a728bc
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-30 11:14:19 -0300

    Adding text camp at app

--- app/src/main/java/com/github/matheusadsantos/aluvery/ui/theme/screen/HomeScreen.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/theme/screen/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.github.matheusadsantos.aluvery.ui.theme.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,24 +18,30 @@ import com.github.matheusadsantos.aluvery.sampledata.sampleSections
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
 import com.github.matheusadsantos.aluvery.ui.theme.component.ProductsSection
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize()
-            .padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductsSection(
-                    title = title,
-                    products = products
-                )
+    Column {
+        OutlinedTextField(value = "Matheus", onValueChange = { newValue ->
+            Log.d("HomrScreen", "HomeScreen: $newValue")
+        })
+        LazyColumn(
+            Modifier
+                .fillMaxSize()
+                .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(
+                        title = title,
+                        products = products
+                    )
+                }
             }
         }
     }
