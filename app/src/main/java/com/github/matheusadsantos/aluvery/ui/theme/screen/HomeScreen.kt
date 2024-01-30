223773b2f66f958f5f8a0b06df2f4ea94cf4a95e
Matheus Santos <matheus.santos@ftdata.com.br>
2024-01-30 12:21:16 -0300

    Using slot API to modify design from search camp

--- app/src/main/java/com/github/matheusadsantos/aluvery/ui/theme/screen/HomeScreen.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/theme/screen/HomeScreen.kt
@@ -4,11 +4,17 @@ import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,10 +35,28 @@ fun HomeScreen(
 ) {
     Column {
         var text by remember { mutableStateOf("") }
-        OutlinedTextField(value = text, onValueChange = { newValue ->
-            Log.d("HomeScreen", "HomeScreen: $text")
-            text = newValue
-        })
+        OutlinedTextField(
+            value = text, onValueChange = { newValue ->
+                Log.d("HomeScreen", "HomeScreen: $text")
+                text = newValue
+            },
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Icon from lupa"
+                )
+            },
+            label = {
+                Text(text = "Product")
+            },
+            placeholder = {
+                Text("What do you looking for?")
+            }
+        )
         LazyColumn(
             Modifier
                 .fillMaxSize()
