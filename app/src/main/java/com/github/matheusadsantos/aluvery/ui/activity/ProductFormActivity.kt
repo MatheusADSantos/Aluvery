cfede9144b9811c22e83298ce67f8faf0d390f50
Matheus Santos <matheus.santos@ftdata.com.br>
2024-02-06 13:02:54 -0300

    Adding image preview

--- app/src/main/java/com/github/matheusadsantos/aluvery/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/activity/ProductFormActivity.kt
@@ -2,15 +2,20 @@ package com.github.matheusadsantos.aluvery.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
@@ -22,9 +27,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.github.matheusadsantos.aluvery.R
 import com.github.matheusadsantos.aluvery.model.Product
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
 import java.math.BigDecimal
@@ -51,9 +60,11 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        Spacer(modifier = Modifier)
         Text(
             "Creating product",
             Modifier.fillMaxWidth(),
@@ -62,6 +73,18 @@ fun ProductFormScreen() {
 
         var url by remember {
             mutableStateOf("")
+        }
+        if (url.isNotBlank()){
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(R.drawable.placeholder),
+                error = painterResource(R.drawable.placeholder),
+            )
         }
         TextField(value = url, onValueChange = {
             url = it
@@ -114,6 +137,7 @@ fun ProductFormScreen() {
         }, Modifier.fillMaxWidth()) {
             Text("Save")
         }
+        Spacer(modifier = Modifier)
     }
 }
 
