5e52d54bdfa82860655357927aca217fdd7eae17
Matheus Santos <matheus.santos@ftdata.com.br>
2024-02-06 10:08:12 -0300

    Adding form camps

--- app/src/main/java/com/github/matheusadsantos/aluvery/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/activity/ProductFormActivity.kt
@@ -3,12 +3,20 @@ package com.github.matheusadsantos.aluvery.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
 
-class ProductFormActivity: ComponentActivity() {
+class ProductFormActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +25,51 @@ class ProductFormActivity: ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    Text("Product Form...")
+                    ProductFormScreen()
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductFormScreen() {
+    Column {
+        Text("Creating product")
+
+        var url by remember {
+            mutableStateOf("")
+        }
+        TextField(value = url, onValueChange = {
+            url = it
+        })
+
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        })
+
+        var price by remember {
+            mutableStateOf("")
+        }
+        TextField(value = price, onValueChange = {
+            price = it
+        })
+
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(value = description, onValueChange = {
+            description = it
+        })
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    ProductFormScreen()
 }
