e71563ab6941e084208ab01d5deacbf1540b383e
Matheus Santos <matheus.santos@ftdata.com.br>
2024-02-06 12:07:46 -0300

    Customizing form fields

--- app/src/main/java/com/github/matheusadsantos/aluvery/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/activity/ProductFormActivity.kt
@@ -3,7 +3,14 @@ package com.github.matheusadsantos.aluvery.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +20,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.matheusadsantos.aluvery.ui.theme.AluveryTheme
 
 class ProductFormActivity : ComponentActivity() {
@@ -35,14 +45,25 @@ class ProductFormActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductFormScreen() {
-    Column {
-        Text("Creating product")
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            "Creating product",
+            Modifier.fillMaxWidth(),
+            fontSize = 28.sp
+        )
 
         var url by remember {
             mutableStateOf("")
         }
         TextField(value = url, onValueChange = {
             url = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text("Url Image")
         })
 
         var name by remember {
@@ -50,6 +71,8 @@ fun ProductFormScreen() {
         }
         TextField(value = name, onValueChange = {
             name = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text("Name")
         })
 
         var price by remember {
@@ -57,6 +80,8 @@ fun ProductFormScreen() {
         }
         TextField(value = price, onValueChange = {
             price = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text("Price")
         })
 
         var description by remember {
@@ -64,7 +89,15 @@ fun ProductFormScreen() {
         }
         TextField(value = description, onValueChange = {
             description = it
+        },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp), label = {
+            Text("Description")
         })
+        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+            Text("Save")
+        }
     }
 }
 
