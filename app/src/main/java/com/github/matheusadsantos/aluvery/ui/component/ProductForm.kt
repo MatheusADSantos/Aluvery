f9aceae9add4f31357023cc95eae1337d6e4de7c
Matheus Santos <matheus.santos@ftdata.com.br>
2024-02-07 23:49:39 -0300

    Abstracting composable product form to separete file

---
+++ app/src/main/java/com/github/matheusadsantos/aluvery/ui/component/ProductForm.kt
@@ -0,0 +1,175 @@
+package com.github.matheusadsantos.aluvery.ui.component
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.github.matheusadsantos.aluvery.R
+import com.github.matheusadsantos.aluvery.model.Product
+import java.math.BigDecimal
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductFormScreen(onSaveProduct: (product: Product) -> Unit = {}) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Spacer(modifier = Modifier)
+        Text(
+            "Creating product",
+            Modifier.fillMaxWidth(),
+            fontSize = 28.sp
+        )
+
+        var url by remember {
+            mutableStateOf("")
+        }
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(R.drawable.placeholder),
+                error = painterResource(R.drawable.placeholder),
+            )
+        }
+        TextField(value = url, onValueChange = {
+            url = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text("Url Image")
+        },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next // to go next step
+            )
+        )
+
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text("Name")
+        },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next, // to go next step
+                capitalization = KeyboardCapitalization.Words
+            )
+        )
+
+        var price by remember { mutableStateOf("") }
+        var isPriceError by remember { mutableStateOf(false) }
+        TextField(
+            value = price,
+            onValueChange = {
+                isPriceError = try {
+                    BigDecimal(it)
+                    false
+                } catch (e: IllegalArgumentException) {
+                    it.isNotEmpty()
+                } catch (e: NumberFormatException) {
+                    Log.e("ProductFormActivity", "ProductFormScreen: ${e.message}")
+                    it.isNotEmpty()
+                }
+                price = it
+            },
+            Modifier.fillMaxWidth(),
+            isError = isPriceError,
+            label = {
+                Text("Price")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next, // to go next step
+            ),
+        )
+        if (isPriceError) {
+            Text(
+                text = "Price must be decimal",
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+        }
+
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = description, onValueChange = {
+                description = it
+            },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp), label = {
+                Text("Description")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Sentences
+            )
+        )
+        Button(onClick = {
+            val convertedPrice = try {
+                BigDecimal(price)
+            } catch (e: NumberFormatException) {
+                BigDecimal.ZERO
+            }
+            val product = Product(
+                name,
+                convertedPrice,
+                url,
+                description
+            )
+            onSaveProduct(product)
+            Log.d("ProductFormActivity", "ProductFormScreen: $product")
+        }, Modifier.fillMaxWidth()) {
+            Text("Save")
+        }
+        Spacer(modifier = Modifier)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    ProductFormScreen()
+}
